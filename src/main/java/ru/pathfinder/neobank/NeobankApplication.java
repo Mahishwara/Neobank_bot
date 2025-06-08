29366bcc54e92a2ee9ad924df00d119148da4783
sev <sev@ap-soft.ru>
2025-06-08 12:37:29 +0500

    init commit

---
+++ src/main/java/ru/pathfinder/neobank/NeobankApplication.java
@@ -0,0 +1,13 @@
+package ru.pathfinder.neobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeobankApplication.class, args);
+    }
+
+}
